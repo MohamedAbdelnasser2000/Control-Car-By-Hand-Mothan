1a7215730e66baa24c698fcd2f67fb113602a02a
Mohamed Nasser <mnkq2000@gmail.com>
2022-06-05 22:05:50 +0200

    Amit Project

---
+++ Interfacing_Module_D26/Interfacing_Module_D26/Debug/APP/MAIN/main.d
@@ -0,0 +1,188 @@
+APP/MAIN/main.d APP/MAIN/main.o: ../APP/MAIN/main.c ../APP/MAIN/main.h \
+ ../MCAL/CPUConfiguration/CPU_Configurations.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../MCAL/CPUConfiguration/BIT_Math.h ../MCAL/CPUConfiguration/STD_Types.h \
+ ../MCAL/DIO/DIO_Private.h ../MCAL/DIO/DIO_Configuration.h \
+ ../MCAL/DIO/DIO_Address.h ../MCAL/Interrupts/Interrupt_Private.h \
+ ../MCAL/Interrupts/Interrupt_Configuration.h \
+ ../MCAL/Interrupts/Interrupt_Address.h ../MCAL/ADC/ADC_Private.h \
+ ../MCAL/ADC/ADC_Configuration.h ../MCAL/ADC/ADC_Address.h \
+ ../MCAL/Timer0/Timer0_Private.h ../MCAL/Timer0/PWM0_Private.h \
+ ../MCAL/Timer0/PWM0_Configuration.h \
+ ../MCAL/Timer0/Timer0_Configuration.h ../MCAL/Timer0/Timer0_Address.h \
+ ../MCAL/WatchDogTimer/WDT_Private.h \
+ ../MCAL/WatchDogTimer/WDT_Configuration.h \
+ ../MCAL/WatchDogTimer/WDT_Address.h ../MCAL/UART/UART_Private.h \
+ ../MCAL/UART/UART_Configuration.h ../MCAL/UART/UART_Address.h \
+ ../MCAL/SPI/SPI_Private.h ../MCAL/SPI/SPI_Configuration.h \
+ ../MCAL/SPI/SPI_Address.h ../HAL/LED/LED_Private.h \
+ ../HAL/LED/LED_Configuration.h ../HAL/LED0/LED0_Private.h \
+ ../HAL/LED0/LED0_Configuration.h ../HAL/LED1/LED1_Private.h \
+ ../HAL/LED1/LED1_Configuration.h ../HAL/LED2/LED2_Private.h \
+ ../HAL/LED2/LED2_Configuration.h ../HAL/RELAY/Relay_Private.h \
+ ../HAL/RELAY/Relay_Configuration.h ../HAL/BUZZER/Buzzer_Private.h \
+ ../HAL/BUZZER/Buzzer_Configuration.h ../HAL/SEVSEG/SEVSEG_Private.h \
+ ../HAL/SEVSEG/SEVSEG_Configuration.h ../HAL/LCD/LCD_Private.h \
+ ../HAL/LCD/LCD_Configuration.h ../HAL/BTN0/BTN0_Private.h \
+ ../HAL/BTN0/BTN0_Configuration.h ../HAL/KEYPAD/Keypad_Private.h \
+ ../HAL/KEYPAD/Keypad_Configuration.h ../HAL/DC_Motor/Motor_Private.h \
+ ../HAL/DC_Motor/Motor_Configuration.h ../HAL/Servo_Motor/Servo_Private.h \
+ ../HAL/Servo_Motor/Servo_Configuration.h
+
+../APP/MAIN/main.h:
+
+../MCAL/CPUConfiguration/CPU_Configurations.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\iom32.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../MCAL/CPUConfiguration/BIT_Math.h:
+
+../MCAL/CPUConfiguration/STD_Types.h:
+
+../MCAL/DIO/DIO_Private.h:
+
+../MCAL/DIO/DIO_Configuration.h:
+
+../MCAL/DIO/DIO_Address.h:
+
+../MCAL/Interrupts/Interrupt_Private.h:
+
+../MCAL/Interrupts/Interrupt_Configuration.h:
+
+../MCAL/Interrupts/Interrupt_Address.h:
+
+../MCAL/ADC/ADC_Private.h:
+
+../MCAL/ADC/ADC_Configuration.h:
+
+../MCAL/ADC/ADC_Address.h:
+
+../MCAL/Timer0/Timer0_Private.h:
+
+../MCAL/Timer0/PWM0_Private.h:
+
+../MCAL/Timer0/PWM0_Configuration.h:
+
+../MCAL/Timer0/Timer0_Configuration.h:
+
+../MCAL/Timer0/Timer0_Address.h:
+
+../MCAL/WatchDogTimer/WDT_Private.h:
+
+../MCAL/WatchDogTimer/WDT_Configuration.h:
+
+../MCAL/WatchDogTimer/WDT_Address.h:
+
+../MCAL/UART/UART_Private.h:
+
+../MCAL/UART/UART_Configuration.h:
+
+../MCAL/UART/UART_Address.h:
+
+../MCAL/SPI/SPI_Private.h:
+
+../MCAL/SPI/SPI_Configuration.h:
+
+../MCAL/SPI/SPI_Address.h:
+
+../HAL/LED/LED_Private.h:
+
+../HAL/LED/LED_Configuration.h:
+
+../HAL/LED0/LED0_Private.h:
+
+../HAL/LED0/LED0_Configuration.h:
+
+../HAL/LED1/LED1_Private.h:
+
+../HAL/LED1/LED1_Configuration.h:
+
+../HAL/LED2/LED2_Private.h:
+
+../HAL/LED2/LED2_Configuration.h:
+
+../HAL/RELAY/Relay_Private.h:
+
+../HAL/RELAY/Relay_Configuration.h:
+
+../HAL/BUZZER/Buzzer_Private.h:
+
+../HAL/BUZZER/Buzzer_Configuration.h:
+
+../HAL/SEVSEG/SEVSEG_Private.h:
+
+../HAL/SEVSEG/SEVSEG_Configuration.h:
+
+../HAL/LCD/LCD_Private.h:
+
+../HAL/LCD/LCD_Configuration.h:
+
+../HAL/BTN0/BTN0_Private.h:
+
+../HAL/BTN0/BTN0_Configuration.h:
+
+../HAL/KEYPAD/Keypad_Private.h:
+
+../HAL/KEYPAD/Keypad_Configuration.h:
+
+../HAL/DC_Motor/Motor_Private.h:
+
+../HAL/DC_Motor/Motor_Configuration.h:
+
+../HAL/Servo_Motor/Servo_Private.h:
+
+../HAL/Servo_Motor/Servo_Configuration.h:
